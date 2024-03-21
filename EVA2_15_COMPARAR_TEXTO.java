a2acc992f74f162498f227731370036908bbbbc5
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2024-03-21 04:43:14 -0600

    Add files via upload

---
+++ EVA2_15_COMPARAR_TEXTO.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.eva2_15_comparar_texto;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author andyl
+ */
+public class EVA2_15_COMPARAR_TEXTO {
+
+    public static void main(String[] args) {
+        //COMPARAR TEXTO EN JAVA
+        
+        String dia;
+        Scanner captu = new Scanner(System.in);
+        
+        System.out.println("Introduce el dia (Lunes, Martes etc...");
+        dia = captu.nextLine();
+        
+        
+        if(dia.equals("domingo"))//Asi se compara al ser texto, No se agrega ==
+            System.out.println("1");
+        else if (dia.equals("lunes"))
+            System.out.println("2");
+        else if (dia.equals("martes"))
+            System.out.println("3");
+        else if (dia.equals("miercoles"))
+            System.out.println("4");
+        else if (dia.equals("jueves"))
+            System.out.println("5");
+        else if (dia.equals("viernes"))
+            System.out.println("6");
+        else if (dia.equals("sabado"))
+            System.out.println("7");
+        
+        else
+            System.out.println("otro dia");
+    }
+}
