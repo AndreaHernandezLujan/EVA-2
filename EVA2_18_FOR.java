679366600c75bc8b2cba2acce1bb13c67043babf
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2024-04-10 00:29:26 -0600

    Add files via upload

---
+++ EVA2_18_FOR.java
@@ -0,0 +1,32 @@
+
+package eva2_18_for;
+
+
+public class EVA2_18_FOR {
+
+    
+    public static void main(String[] args) {
+        //INICIO; CONDICION; INCREMENTO; DECREMENTO
+        for(int i = 1; i <= 10; i++){
+            //LLAVES SON OPCIONALES, USENLAS CUANDO
+            //TENGAN  MAS DE UNA INSTRUCCION A REPETIR
+            System.out.print(i + " - ");
+    }
+        System.out.println("");
+    for(int i = 10; i >= 1; i--){
+            //LLAVES SON OPCIONALES, USENLAS CUANDO
+            //TENGAN  MAS DE UNA INSTRUCCION A REPETIR
+            System.out.print(i + " - ");
+    }
+            //IMPRIMIR LOS NUMEROS PARES ENTRE O Y 100
+            //0,2 4 6 8 10
+            System.out.println("");
+            for(int i = 0; i <= 100; i+=2)
+                System.out.print(i + "-");
+            }
+}
+    
+
+
+
+
