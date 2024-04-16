e55245ca56de8a5f1d2024f2c84dae02bac08b11
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2024-04-16 01:13:45 -0600

    Add files via upload

---
+++ EVA2_21_WHILE.java
@@ -0,0 +1,18 @@
+
+package eva2_21_while;
+
+
+public class EVA2_21_WHILE {
+
+    
+    public static void main(String[] args) {
+       //FOR --> CUENTA
+       //WHILE Y DO WHILE --> CON CONDICIONES
+       int cont = 1;
+       while (cont <= 10) { //solo tiene la condicion
+           System.out.print (cont + " - ");
+           cont++;
+       }
+    }
+    
+}
