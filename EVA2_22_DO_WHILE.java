4e212ef981bad8b0bbb6dfdfcd302f8ae0565fa4
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2024-04-16 01:16:06 -0600

    Add files via upload

---
+++ EVA2_22_DO_WHILE.java
@@ -0,0 +1,16 @@
+
+package eva2_22_do_while;
+
+
+public class EVA2_22_DO_WHILE {
+
+    
+    public static void main(String[] args) {
+        int cont = 1;
+        do{
+            System.out.print(cont + " - ");
+            cont++;
+        }while (cont <= 10);
+    }
+    
+}
