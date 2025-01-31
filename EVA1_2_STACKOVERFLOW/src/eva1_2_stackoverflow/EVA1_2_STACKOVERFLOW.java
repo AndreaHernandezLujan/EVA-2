00700931f203970b908171a86780d0f9781b0044
AndreaHernandezLujan <159495006+AndreaHernandezLujan@users.noreply.github.com>
2025-01-30 22:05:24 -0600

    Add files via upload

---
+++ EVA1_2_STACKOVERFLOW/src/eva1_2_stackoverflow/EVA1_2_STACKOVERFLOW.java
@@ -0,0 +1,15 @@
+
+package eva1_2_stackoverflow;
+
+//Andrea Valeria Hernandez Lujan
+public class EVA1_2_STACKOVERFLOW {
+
+    
+    public static void main(String[] args) {
+        A();
+    }
+    public static void A(){
+        A();
+    }
+    
+}
